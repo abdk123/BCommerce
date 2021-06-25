080b3ae970512d3081181bb9a79122483ce6d1c9
Ali.Ali <ai_essa_ali@outlook.com>
2021-06-25 03:47:32 +0300

    shopping cart

---
+++ Presentation/Nop.Web/Models/Mobile/ShoppingCart/OrderTotalMobModel.cs
@@ -0,0 +1,23 @@
+﻿using Nop.Web.Framework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nop.Web.Models.Mobile.ShoppingCart
+{
+    public class OrderTotalMobModel : BaseNopModel
+    {
+        public OrderTotalMobModel()
+        {
+        }
+
+        public string SubTotal { get; set; }
+
+        public string SubTotalDiscount { get; set; }
+
+        public string Shipping { get; set; }
+        public string OrderTotalDiscount { get; set; }
+        public string OrderTotal { get; set; }
+    }
+}
